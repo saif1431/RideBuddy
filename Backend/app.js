c0b940fd9591bc1d64063d256bc8c392e5d9521a
Saif ur Rehman <buttsaif539@gmail.com>
2025-02-05 19:16:16 +0500

    Create Database

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectToDB = require('./db/db')
 
 const app = express();
+connectToDB();
+
 app.use(cors());
 
+// Use the imported connectDB function
+
 app.get("/", (req, res) => {
-      res.send("Hello World");
-})
+    res.send("Hello World");
+});
---
+++ Backend/db/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+function connectToDB () {
+      mongoose.connect(process.env.MONGODB_URI)
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.log(err));
+}
